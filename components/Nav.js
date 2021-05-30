828ccfbacac0c97dd3412dd4268806c68b4b04c3
Mihael Ištvan <mihael.istvan@gmail.com>
2021-05-30 21:57:27 +0200

    logout button

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,38 @@
-export default function Nav({ props }) {
+import React from "react";
+import { AuthContext } from "../pages/_app.js";
+
+export default function Nav(props) {
+  const { dispatch } = React.useContext(AuthContext);
+
+  function Logout() {
+    if (props.isAuthenticated) {
+      return (
+        <p
+          onClick={handleLogoutClick}
+          className="p-4 hover:bg-gray-100 cursor-pointer"
+        >
+          Log Out
+        </p>
+      );
+    }
+    return null;
+  }
+
+  function handleLogoutClick(e) {
+    e.preventDefault();
+    console.log("loggin out");
+    dispatch({
+      type: "LOGOUT",
+    });
+  }
+
   return (
     <nav className="h-12 bg-gray-300 flex items-center justify-between px-4">
-      {props}
-      <p>logo</p>
-      <p>log out</p>
+      <div className="flex items-center">
+        <p className="mr-2">Filejet</p>
+        <img src="/logo.svg" alt="logo" className="w-8 h-8" />
+      </div>
+      <Logout />
     </nav>
   );
 }
